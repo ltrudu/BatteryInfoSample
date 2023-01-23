0b5c6034eb9c09b3e271877bb3289233c3bcfcd4
Trudu Laurent <laurent@trudu.com>
2023-01-23 11:24:32 +0100

    SDK 32 Update Gradle 7.4 update AndroidX Update

--- app/src/main/java/com/symbol/batteryinfosample/MainActivity.java
+++ app/src/main/java/com/symbol/batteryinfosample/MainActivity.java
@@ -5,7 +5,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.graphics.Typeface;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.util.TypedValue;
@@ -18,7 +18,6 @@ import android.widget.TextView;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
 
